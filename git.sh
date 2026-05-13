6e53a8cefa4660bf665f232519ec568749c7dc9c
Tom Keene <tom@theanthillsocial.co.uk>
2026-05-13 17:10:11 +0100

    small update

--- git.sh
+++ git.sh
@@ -1,3 +1,11 @@
+#if find . -type f -size +100M -print -quit | grep -q .; then
+#  echo "❌ ERROR: Files over 100MB found:"
+#  find . -type f -size +100M -exec ls -lh {} \;
+#  exit 1
+#fi
+
+#echo "✅ No large files found. Proceeding..."
+
 git add -A
 git commit -m "small update"
 git push origin main
